8cf030aa388ee29561fc509f7e8378232ee4a0b6
Jaime <jaimelanglois.kyes@Student.Sl.On.Ca>
2025-12-26 03:02:14 -0500

    initial commit

---
+++ DinnerDecider/src/data/food.js
@@ -0,0 +1,52 @@
+const foods = {
+    pizza: {
+        id: 1,
+        name: "Pizza",
+        image: "https://cdn.pixabay.com/photo/2024/04/18/10/41/ai-generated-8704060_1280.jpg",
+},
+    steak: {
+    id: 2,
+    name: "Steak",
+    image: "https://cdn.pixabay.com/photo/2016/03/05/19/02/abstract-1238247_1280.jpg",
+},
+porkChops: {
+    id: 3,
+    name: "Pork Chops",
+    image: "https://cdn.pixabay.com/photo/2017/07/16/10/43/pork-2507268_1280.jpg",
+},
+meatloaf: {
+    id: 4,
+    name: "Meatloaf",
+    image: "https://cdn.pixabay.com/photo/2018/06/18/16/05/meatloaf-3483050_1280.jpg",
+},
+burgers: {
+    id: 5,
+    name: "Burgers",
+    image: "https://cdn.pixabay.com/photo/2014/10/23/18/05/burger-500054_1280.jpg",
+},
+meatballs: {
+    id: 6,
+    name: "Meatballs",
+    image: "https://cdn.pixabay.com/photo/2017/05/07/08/56/meatballs-2291908_1280.jpg",
+},
+fish: {
+    id: 7,
+    name: "Fish",
+    image: "https://cdn.pixabay.com/photo/2016/11/18/15/05/fish-1835011_1280.jpg",
+},
+shrimp: {
+    id: 8,
+    name: "Shrimp",
+    image: "https://cdn.pixabay.com/photo/2016/03/05/19/02/shrimp-1238248_1280.jpg",
+},
+turkeyDishes: {
+    id: 9,
+    name: "Turkey Dishes",
+    image: "https://cdn.pixabay.com/photo/2016/11/29/09/32/turkey-1868527_1280.jpg",
+},
+chicken: {
+    id: 10,
+    name: "Chicken",
+    image: "https://cdn.pixabay.com/photo/2017/06/02/18/24/chicken-2364693_1280.jpg",
+},
+}
